8a9d226132a85f68b921d4bc2df1bafc1abb2ba5
KingsleyMatthew <kedet1@byupathway.edu>
2026-05-10 09:11:49 +0100

    update

--- scripts/dates.js
+++ scripts/dates.js
@@ -1,6 +1,5 @@
-const year = document.querySelector("#currentyear");
-
-year.textContent = new Date().getFullYear();
+document.querySelector("#currentyear").textContent =
+  new Date().getFullYear();
 
 document.querySelector("#lastModified").textContent =
-  `Last Modification: ${document.lastModified}`;
+  document.lastModified;
