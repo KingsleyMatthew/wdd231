de3a046b059b1871dbd937197f7ecd876c9cec45
KingsleyMatthew <kedet1@byupathway.edu>
2026-06-14 12:30:45 +0100

    inline javascript files deleted

--- finalproject/scripts/main.js
+++ finalproject/scripts/main.js
@@ -2,3 +2,10 @@ import "./navigation.js";
 
 document.querySelector("#year").textContent =
   new Date().getFullYear();
+
+document.addEventListener("DOMContentLoaded", () => {
+    const yearElement = document.getElementById("year");
+    if (yearElement) {
+        yearElement.textContent = new Date().getFullYear();
+    }
+});
